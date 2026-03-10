f9f0d83daa00ef62793dce118637b1dc1f474a5d
solomonbestz <kingsolodawarrior@gmail.com>
2026-03-10 23:21:57 +0100

    feat: Interactivity

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,5 +1,17 @@
-
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  print('Hello Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    print('Hello, Dart!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
 }
