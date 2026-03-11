d725f2d76cd27cede8e11bd236499f4e3a47d3c9
solomonbestz <kingsolodawarrior@gmail.com>
2026-03-11 14:33:58 +0100

    feat: added the stdin to receive user's input

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -20,5 +22,14 @@ void printUsage() {
 }
 
 void searchWikipedia(List<String>? arguments) {
-  print('searchWikipedia received arguments: $arguments');
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article title: $articleTitle');
 }
